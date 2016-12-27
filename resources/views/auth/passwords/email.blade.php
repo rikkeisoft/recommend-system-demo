7a711b32b8d4efd17e24e0849c323de4d3e80b6c
dungtq <dungtq@rikkeisoft.com>
2016-12-27 17:14:19 +0700

    Create Auth

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!--BEGIN INCLUDE CSS-->
+        {{ Html::style('https://fonts.googleapis.com/css?family=Roboto') }}
+        {{ Html::style('css/app.css') }}
+        {{ Html::style('css/font-awesome.min.css') }}
+        {{ Html::style('css/common.css') }}
+        @yield('css')
+        <!--END INCLUDE CSS-->
+    </head>
+    <div class="container auth-container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Reset Password</div>
+                    <div class="panel-body">
+                        @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Send Password Reset Link
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--BEGIN INCLUDE SCRIPT-->
+    {{ Html::script('js/jquery-1.12.4.min.js') }}
+    {{ Html::script('js/app.js') }}
+    {{ Html::script('js/common.js') }}
+    @yield('script')
+
+    <script>
+        $(document).ready(function () {
+            Common.init();
+        });
+    </script>
+    <!--END INCLUDE SCRIPT-->
+</body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!--BEGIN INCLUDE CSS-->
+        {{ Html::style('https://fonts.googleapis.com/css?family=Roboto') }}
+        {{ Html::style('css/app.css') }}
+        {{ Html::style('css/font-awesome.min.css') }}
+        {{ Html::style('css/common.css') }}
+        @yield('css')
+        <!--END INCLUDE CSS-->
+    </head>
+    <body>
+        <div class="container auth-container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Register</div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                                {{ csrf_field() }}
+
+                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                    <label for="name" class="col-md-4 control-label">Name</label>
+
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                        @if ($errors->has('name'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                        @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <label for="password" class="col-md-4 control-label">Password</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control" name="password" required>
+
+                                        @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Register
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--BEGIN INCLUDE SCRIPT-->
+        {{ Html::script('js/jquery-1.12.4.min.js') }}
+        {{ Html::script('js/app.js') }}
+        {{ Html::script('js/common.js') }}
+        @yield('script')
+
+        <script>
+            $(document).ready(function () {
+                Common.init();
+            });
+        </script>
+        <!--END INCLUDE SCRIPT-->
+    </body>
+</html>
